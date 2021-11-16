1a9518a046498d7612a6453e21fa8f7bff056773
cksdud7158 <cksdud7158@naver.com>
2021-11-16 17:23:10 +0900

    firebase init

---
+++ src/components/layouts/layoutNormal.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container> normal </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class layoutNoraml extends Vue {}
+</script>
+
+<style lang="scss"></style>
